5611ea89592c6155cec606650e7b51391851782a
Mihalis Tsiakkas <m.tsiakkas@mntx.eu>
2025-07-27 08:42:44 +0300

    test: add error envelope tests

--- error_test.go
+++ error_test.go
@@ -2,6 +2,7 @@ package rgroup
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 	"testing"
 )
@@ -71,6 +72,93 @@ func TestError(t *testing.T) {
 
 	Config.Reset()
 
+}
+
+func TestErrorEnvelope(t *testing.T) {
+	err := HandlerError{
+		HTTPStatus: http.StatusNotAcceptable,
+	}
+
+	target := Envelope{
+		Data: nil,
+		Status: EnvelopeStatus{
+			HTTPStatus: http.StatusNotAcceptable,
+			Message:    nil,
+			Error:      toPtr(http.StatusText(http.StatusNotAcceptable)),
+		},
+	}
+	envCompare(t, err.ToEnvelope(), &target)
+
+	err.Response = "test response"
+	target.Status.Error = toPtr("test response")
+	envCompare(t, err.ToEnvelope(), &target)
+
+	Config.SetForwardLogMessage(true)
+	target.Status.Message = toPtr(err.Error())
+	envCompare(t, err.ToEnvelope(), &target)
+
+	err.err = fmt.Errorf("test error")
+	target.Status.Message = toPtr(err.Error())
+	envCompare(t, err.ToEnvelope(), &target)
+
+	err.HTTPStatus = 333
+	err.Response = ""
+	target.Status.Error = toPtr("unkown error")
+	target.Status.HTTPStatus = 333
+	envCompare(t, err.ToEnvelope(), &target)
+
+	Config.Reset()
+}
+
+func envCompare(t *testing.T, env *Envelope, target *Envelope) {
+	switch {
+	case env.Status.HTTPStatus != target.Status.HTTPStatus:
+		t.Logf("unexpected status: %d", env.Status.HTTPStatus)
+		t.Fail()
+	case env.Data != target.Data:
+		t.Logf("unexpected data: %s", env.Data)
+		t.Fail()
+	case !strPtrCompare(env.Status.Message, target.Status.Message):
+		t.Log("message not match")
+		if target.Status.Message != nil {
+			t.Logf("target: %s", *target.Status.Message)
+		} else {
+			t.Log("target: nil")
+		}
+		if env.Status.Message != nil {
+			t.Logf("got: %s", *env.Status.Message)
+		} else {
+			t.Log("got: nil")
+		}
+		t.Fail()
+	case !strPtrCompare(env.Status.Error, target.Status.Error):
+		t.Log("error not match")
+		if target.Status.Error != nil {
+			t.Logf("target: %s", *target.Status.Error)
+		} else {
+			t.Log("target: nil")
+		}
+		if env.Status.Error != nil {
+			t.Logf("got: %s", *env.Status.Error)
+		} else {
+			t.Log("got: nil")
+		}
+	}
+}
+
+func strPtrCompare(s1 *string, s2 *string) bool {
+	switch {
+	case s1 == nil && s2 == nil:
+		return true
+	case s1 == nil && s2 != nil || s1 != nil && s2 == nil:
+		return false
+	case *s1 != *s2:
+		return false
+	case *s1 == *s2:
+		return true
+	default:
+		return false
+	}
 }
 
 func errorCompare(e1 HandlerError, e2 HandlerError) bool {
