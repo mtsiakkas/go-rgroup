3e2c8822dced9dc314ad7e4c4761cca913aedcd1
Mihalis Tsiakkas <m.tsiakkas@mntx.eu>
2026-04-19 11:39:44 +0300

    docs: add HandlerMux example

---
+++ examples/servemux/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/mtsiakkas/go-rgroup"
+)
+
+func main() {
+
+	// Define handler groups
+	g1 := rgroup.NewWithHandlers(rgroup.HandlerMap{
+		http.MethodGet:  handleGet1,
+		http.MethodPost: handlePost1,
+	})
+
+	g2 := rgroup.NewWithHandlers(rgroup.HandlerMap{
+		http.MethodGet:  handleGet2,
+		http.MethodPost: handlePost2,
+	})
+
+	// Create new http.ServeMux
+	r := rgroup.NewServeMux()
+
+	// Add generated http.Handler/http.HandlerFunc to r
+	r.Handle("/g1", g1)
+	r.Handle("/g2", g2)
+	r.AddMiddleware(middleware)
+
+	// Start http server
+	fmt.Println("listening on localhost:3000")
+	if err := http.ListenAndServe("localhost:3000", r); err != nil {
+		log.Fatalf("failed to start server: %s", err)
+	}
+}
+
+// rgroup.Handler for GET method on g1
+func handleGet1(w http.ResponseWriter, req *http.Request) (*rgroup.HandlerResponse, error) {
+	res := rgroup.Response("hello from GET 1 handler").
+		WithMessage("GET 1 request - said hello").
+		WithHTTPStatus(http.StatusAccepted)
+
+	return res, nil
+}
+
+// rgroup.Handler for POST method on g1
+// http.StatusNotImplemented error
+func handlePost1(w http.ResponseWriter, req *http.Request) (*rgroup.HandlerResponse, error) {
+	err := rgroup.Error(http.StatusNotImplemented).
+		WithResponse("POST 1 method not implemented").
+		WithMessage("POST 1 request - not implemented")
+
+	return nil, err
+}
+
+// rgroup.Handler for GET method on g2
+func handleGet2(w http.ResponseWriter, req *http.Request) (*rgroup.HandlerResponse, error) {
+	res := rgroup.Response("hello from GET 2 handler").
+		WithMessage("GET 2 request - said hello").
+		WithHTTPStatus(http.StatusAccepted)
+
+	return res, nil
+}
+
+// rgroup.Handler for POST method on g2
+// http.StatusNotImplemented error
+func handlePost2(w http.ResponseWriter, req *http.Request) (*rgroup.HandlerResponse, error) {
+	ctx := req.Context()
+	message := ctx.Value("mid-ctx")
+	fmt.Println(message)
+	return rgroup.Response(message), nil
+}
+
+func middleware(h rgroup.Handler) rgroup.Handler {
+	return func(w http.ResponseWriter, req *http.Request) (*rgroup.HandlerResponse, error) {
+		ctx := context.WithValue(req.Context(), "mid-ctx", "hello from context middleware")
+		return h(w, req.WithContext(ctx))
+	}
+}
